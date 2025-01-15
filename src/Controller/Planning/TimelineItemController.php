e6fea4dc4fa929caa852a68c59c487523938a8a9
Robin Aldasoro <robin.aldasoro@gmail.com>
2025-01-15 21:26:03 +0100

    fix(timeline): wrong route name for timeline item

--- src/Controller/Planning/TimelineItemController.php
+++ src/Controller/Planning/TimelineItemController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/timeline-item/{timelineItem}}', name: 'app_timeline_item', methods: ['GET'])]
+#[Route('/timeline-item/{timelineItem}', name: 'app_timeline_item', methods: ['GET'])]
 
 class TimelineItemController extends AbstractController
 {
